26aa0ae6d48b4ba6dcb5cf7522baf12d8545eeab
RomainVenel <romain.venel.sio@gmail.com>
2022-04-28 16:15:24 +0200

    Initialisation architecture React + creation grid

---
+++ src/components/Grid.jsx
@@ -0,0 +1,17 @@
+import './grid.scss';
+import './Row'
+import Row from "./Row";
+
+function Grid() {
+    return(
+        <div id={'grid'}>
+            <Row/>
+            <Row/>
+            <Row/>
+            <Row/>
+            <Row/>
+            <Row/>
+        </div>
+    )
+}
+export default Grid
---
+++ src/components/Row.jsx
@@ -0,0 +1,8 @@
+import './grid.scss';
+
+function Row() {
+    return(
+        <div className={'row'}>Row</div>
+    )
+}
+export default Row
