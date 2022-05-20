d5be5bc15444c6fd39e53ae730fcc1a8e9519f90
RomainVenel <romain.venel.sio@gmail.com>
2022-05-20 08:08:19 +0200

    Sauvegarde des mots rentrés par l'utilisateur à chaque ligne

--- src/components/Grid/Row.jsx
+++ src/components/Grid/Row.jsx
@@ -5,8 +5,12 @@ import * as React from "react";
 
 function Row(props) {
 
-    const {letter, indexCase, indexRow,  placedLetters, oldWords} = React.useContext(WordleContext);
+    const {letter, indexCase, indexRow,  placedLetters, oldWords, setErrorMessage} = React.useContext(WordleContext);
     const numbers = [0, 1, 2, 3, 4];
+
+    function endGame() {
+        setErrorMessage('Jeu terminé');
+    }
 
     const getLetter = (index) => {
         if (props.stateRow) {
@@ -16,8 +20,19 @@ function Row(props) {
             return placedLetters[index];
         } else {
             if (oldWords.length > 0) {
-                if (props.index === indexRow-1) {
-                    return oldWords[indexRow-1][index];
+                if (props.index === indexRow - 1) {
+                    return oldWords[indexRow - 1][index];
+                } else if (props.index === indexRow - 2) {
+                    return oldWords[indexRow - 2][index];
+                } else if (props.index === indexRow - 3) {
+                    return oldWords[indexRow - 3][index];
+                } else if (props.index === indexRow - 4) {
+                    return oldWords[indexRow - 4][index];
+                } else if (props.index === indexRow - 5) {
+                    return oldWords[indexRow - 5][index];
+                } else if (props.index === indexRow - 6) {
+                    endGame();
+                    return oldWords[indexRow - 6][index];
                 }
             }
         }
